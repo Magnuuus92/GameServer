4717f97a9fb36bd1577bfb91d118c1daf5af228d
Magnus <Magnus92.kodehode@gmail.com>
2026-07-09 09:01:31 +0200

    a

---
+++ Program.cs
@@ -0,0 +1,7 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using GameServer.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+//
