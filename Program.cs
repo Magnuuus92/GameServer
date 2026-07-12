1ceb90073f99dec2d3a552926dfa011b8215d4ed
Magnus <Magnus92.kodehode@gmail.com>
2026-07-12 18:20:08 +0200

    p

--- Program.cs
+++ Program.cs
@@ -4,4 +4,36 @@ using Microsoft.IdentityModel.Tokens;
 using GameServer.Services;
 
 var builder = WebApplication.CreateBuilder(args);
-//comebacktothis
+//SERVICES
+builder.Services.AddSingleton<mongoService>();
+
+builder.Services.AddSingleton<JwtService>();
+
+builder.Services.AddControllers();
+//CORS requests from frontend
+var allowedOrigins = builder.Configuration.GetSection("AllowedOrigins").Get<string[]>()
+?? ["http://localhost:5500"];
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy
+        .WithOrigins(allowedOrigins)
+        .AllowedAnyHeader()
+        .AllowAnyMethod();
+    });
+});
+
+//JWT AUTH
+var jwtSecret = builder.Configuration["Jwt:Secret"]
+?? throw new InvalidOperationException("JWT secret not configured.");
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+.AddJwtBearer(options =>
+{
+    options.TokenValidationsParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+    }
+})
